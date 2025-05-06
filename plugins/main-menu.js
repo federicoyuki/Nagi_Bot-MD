f0f1d7bbfa5f88247f98fc0baff12e8e22a2ddc9
federicoyuki <vigevanifede833@gmail.com>
2025-05-06 19:32:42 -0300

    Update main-menu.js
    Signed-off-by: federicoyuki <vigevanifede833@gmail.com>

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -185,7 +185,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       },
     ];
 
-    let img = 'https://qu.ax/JznsE.jpg';
+    let img = 'https://files.catbox.moe/9g20h3.mp4';
     await m.react('⚽️');
 
     await conn.sendMessage(
