dd7cf6a2a3124c96e38719f96d3c709e47459127
federicoyuki <vigevanifede833@gmail.com>
2025-05-06 19:04:39 -0300

    Update info-creador.js
    Signed-off-by: federicoyuki <vigevanifede833@gmail.com>

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;BRAYAN-GT⁩;;\nFN:BRAYAN-GT⁩\nORG:BRAYAN-GT⁩\nTITLE:\nitem1.TEL;waid=50231458537:5218261275256\nitem1.X-ABLabel:BRAYAN-GT\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:BRAYAN-GT⁩\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おBrayan.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;𝘧𝘦𝘥𝘦𝘭𝘢𝘯𝘠𝘛;;\nFN:𝘧𝘦𝘥𝘦𝘭𝘢𝘯𝘠𝘛\nORG:𝘧𝘦𝘥𝘦𝘭𝘢𝘯𝘠𝘛\nTITLE:\nitem1.TEL;waid=5491156178758:5218261275256\nitem1.X-ABLabel:𝘧𝘦𝘥𝘦𝘭𝘢𝘯𝘠𝘛\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:𝘧𝘦𝘥𝘦𝘭𝘢𝘯𝘠𝘛⁩\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: '𝘧𝘦𝘥𝘦𝘭𝘢𝘯𝘠𝘛⁩', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
 handler.tags = ['main']
